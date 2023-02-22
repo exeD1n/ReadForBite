c13864536930d5285ae01dcf4fa0410720864602
Daniil Lutaev <daniillutaev1@gmail.com>
2023-02-22 20:04:00 +0400

    па

--- write.py
+++ write.py
@@ -5,14 +5,9 @@ for i in range(1,10000000):
     L.append(random.randint(1,10000000000000))
 f = open('myfile3.bin', 'wb')
 
-# 2.2. Обход списка и запись данных в файл
 for item in L:
-    # добавить символ '\n', чтобы можно было распознать числа
     s = str(item) + '\n'
-
-    # Метод encode() - конвертирует строку в последовательность байт
     bt = s.encode('utf-8')
-    f.write(bt) # метод write() - запись в файл
-
-# 2.3. Закрыть файл
+    f.write(bt)
+    
 f.close()
